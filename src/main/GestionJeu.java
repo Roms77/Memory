3e8c6ab31ab54251cb211f647510c6147611a4d6
Roms77 <romainmignac@gmail.com>
2021-09-16 16:51:29 +0200

    Revert "Création des classes"
    This reverts commit a1a0d5f44726cedba58ca09e31e026003f8e528b.

--- src/main/GestionJeu.java
+++ src/main/GestionJeu.java
@@ -1,4 +1,33 @@
 package main;
 
 public class GestionJeu {
+
+    private Joueur joueur[];
+    private Grille grille;
+    public GestionJeu(){
+
+    }
+
+    public void initJoueur(){
+
+    }
+    public void choisirNombre(){
+
+    }
+
+    public void jouer(){
+
+
+        while(true){
+            //retourne carte 1
+
+            //retourne carte 2
+
+            //si pas identique alors retourne
+        }
+
+
+
+    }
+
 }
--- src/main/Joueur.java
+++ src/main/Joueur.java
@@ -1,4 +1,26 @@
 package main;
 
+
+
 public class Joueur {
+
+    private int score;
+    private final int numero;
+
+
+    public Joueur(int numero){
+        this.numero=numero;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getNumero(){
+        return numero;
+    }
 }
